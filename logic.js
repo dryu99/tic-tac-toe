47c0263337467bf655f38cc2d23f014af7451b17
Daniel Ryu <rujjiryu99@gmail.com>
2019-06-12 22:50:12 -0700

    refactor displayController to handle DOM manipulation with jQuery instead of JavaScript

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 /**
- * empty = 0
+ * empty = -1
+ * O = 0
  * X = 1
- * O = 2
  */
 
 /* OBJECTS */ 
@@ -24,6 +24,16 @@ const game = (() => {
 	let currentPlayer = 0;
 	const playerO = player(0);
 	const playerX = player(1);	
+	const winStates = [
+		[0,1,2],
+		[3,4,5],
+		[6,7,8],
+		[0,3,6],
+		[1,4,7],
+		[2,5,8],
+		[0,4,8],
+		[2,4,6] 
+	];
 
 	const getCurrentPlayer = () => currentPlayer;
 
@@ -35,7 +45,6 @@ const game = (() => {
 		}
 
 		currentPlayer = Number(!currentPlayer);
-		console.log("playTurn done");
 	};
 
 	return { playTurn, getCurrentPlayer };
@@ -44,52 +53,76 @@ const game = (() => {
 
 // GameBoard module
 const gameBoard = (() => {
-	const matrix = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+	const matrix = [0,-1,-1,-1,-1,-1,-1,-1,-1];
 	
 	const getMatrix = () => matrix;
 
 	return { getMatrix };
 })();
 
-// DisplayController module
-const displayController = (() => {
+$(document).ready(function() {
 
-	const _numToToken = (num) => { 
-	  return num === -1 ? "" : num ? "X" : "O";
-	};
+	// DisplayController module
+	const displayController = (() => {
 
-	const _markCell = (e) => {
-		if (e.target.textContent !== "") return;
+		const _numToToken = (num) => { 
+			return num === -1 ? "" : num ? "X" : "O";
+		};
 
-		let pos = e.target.nodeName.toLowerCase() === "span" ? Array.prototype.indexOf.call(e.target.parentNode.parentNode.children, e.target.parentNode) :
-			Array.prototype.indexOf.call(e.target.parentNode.children, e.target); // this is terrible change div/span selection logic
-		game.playTurn(pos);
+		const _markCell = (e) => {
 
-		e.target.textContent = _numToToken(game.getCurrentPlayer()); 
-		console.log("markCell done");
-	};
+			if (e.currentTarget.textContent !== "") return;
+			// JQUERY
+			// if ($(this).text() !== "") return;
+			
 
-	const render = () => {
-		const gameBoardDiv = document.querySelector(".game-board");
+			// e.target.textContent = _numToToken(game.getCurrentPlayer()); // mark cell with current player token
+			e.currentTarget.textContent = _numToToken(game.getCurrentPlayer());
+			// JQUERY
+			// $(this).text(_numToToken(game.getCurrentPlayer())); 
+			
 
-		for (let i = 0; i < 9; i++) {
-			const cell = document.createElement("div");
-			cell.classList.add(`game-cell`);
-			cell.textContent = _numToToken(gameBoard.getMatrix()[i]);
-			// cell.id = i;
-			cell.addEventListener("click", _markCell);
+			// let pos = e.target.nodeName.toLowerCase() === "span" ? Array.prototype.indexOf.call(e.target.parentNode.parentNode.children, e.target.parentNode) :
+			// 	Array.prototype.indexOf.call(e.target.parentNode.children, e.target); // this is terrible change div/span selection logic
 
-			gameBoardDiv.appendChild(cell);
-		}
-	};
+			// JQUERY
+			// let pos = $(this).prop("tagName").toLowerCase() === "span" ? Array.prototype.indexOf.call($(this).parent().parent().children(), $(this).parent()) :
+			// 	Array.prototype.indexOf.call($(this).parent().children(), $(this));
 
-	return { render };
+			let pos = e.currentTarget.nodeName.toLowerCase() === "span" ? Array.prototype.indexOf.call(e.currentTarget.parentNode.parentNode.children, e.currentTarget.parentNode) :
+			Array.prototype.indexOf.call(e.currentTarget.parentNode.children, e.currentTarget); // this is terrible change div/span selection logic
+			
+			game.playTurn(pos);
+		};
 
-})();
+		const render = () => {
+			// const gameBoardDiv = document.querySelector(".game-board");
+
+			for (let i = 0; i < 9; i++) {
+				const cell = $("<div>").addClass("game-cell");
+				cell.text(_numToToken(gameBoard.getMatrix()[i]));				
+				cell.click(_markCell);
+				// const cell = document.createElement("div");
+				// cell.classList.add(`game-cell`);
+				// cell.textContent = _numToToken(gameBoard.getMatrix()[i]);
+				// cell.id = i;
+				// cell.addEventListener("click", _markCell);
+
+				// gameBoardDiv.appendChild(cell);
+			  $(".game-board").append(cell);
+			}
+		};
+
+		return { render };
+	})();		
+
+	displayController.render();
+});
 
 
 
-displayController.render();
+
+
 
 
 /**
@@ -107,11 +140,16 @@ displayController.render();
  * - 		problem was because there are also spans inside the divs, so clicking on span would just return 0 instead of cell index
  * -    	tried to initially fix by checking to see whether span or div was clicked, and making proper adjustments to parentNode calls
  * - 			was so messy, so i tried ...
+ * 
  * - when I tried to access game.currentPlayer, I would always get 1 even though i had a function call that should change it to 2
  * - 		apparently when you properties put into returned objects stay constant, unless you specifically modify that object.
  * -  	if you modify via a function, it does not modify it, modifys the variable inside the function scope
  * -  	this is really confusing, go check out objects_practice example to refresh ur memory
+ * 
  * - figuring out which module each piece of logic should go to
+ * 
  * - have two click listeners for cell, not sure how to combine them/which module they should be in
- *
+ * 
+ * - can't figure out how to pass in an already declared callback function into .click(), and have access to the targetted DOM node using $(this). 
+ * 			I can only do it using e.currentTarget, but I dont know why $(this) isn't working sigh
  */
