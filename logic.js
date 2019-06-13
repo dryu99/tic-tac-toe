fc688ac51211f32c27500500b1512585c62dffb4
Daniel Ryu <rujjiryu99@gmail.com>
2019-06-12 21:20:39 -0700

    tentatively removed spans and combine cell listeners to increase cohesion

--- logic.js
+++ logic.js
@@ -27,15 +27,7 @@ const game = (() => {
 
 	const getCurrentPlayer = () => currentPlayer;
 
-	const playTurn = (e) => {
-		let pos;
-
-		if (e.target.nodeName.toLowerCase() === "span") { // change this
-			pos = Array.prototype.indexOf.call(e.target.parentNode.parentNode.children, e.target.parentNode);			
-		} else {
-			pos = Array.prototype.indexOf.call(e.target.parentNode.children, e.target);			
-		}
-
+	const playTurn = (pos) => {		
 		if (currentPlayer) {
 			playerX.markAt(pos);
 		} else {
@@ -43,6 +35,7 @@ const game = (() => {
 		}
 
 		currentPlayer = Number(!currentPlayer);
+		console.log("playTurn done");
 	};
 
 	return { playTurn, getCurrentPlayer };
@@ -63,12 +56,17 @@ const displayController = (() => {
 
 	const _numToToken = (num) => { 
 	  return num === -1 ? "" : num ? "X" : "O";
-		 
-		// maybe I could have an num-token object in displayController and just use that
 	};
 
 	const _markCell = (e) => {
-		e.target.firstChild.textContent = _numToToken(game.getCurrentPlayer()); 
+		if (e.target.textContent !== "") return;
+
+		let pos = e.target.nodeName.toLowerCase() === "span" ? Array.prototype.indexOf.call(e.target.parentNode.parentNode.children, e.target.parentNode) :
+			Array.prototype.indexOf.call(e.target.parentNode.children, e.target); // this is terrible change div/span selection logic
+		game.playTurn(pos);
+
+		e.target.textContent = _numToToken(game.getCurrentPlayer()); 
+		console.log("markCell done");
 	};
 
 	const render = () => {
@@ -77,13 +75,9 @@ const displayController = (() => {
 		for (let i = 0; i < 9; i++) {
 			const cell = document.createElement("div");
 			cell.classList.add(`game-cell`);
-			cell.id = i;
+			cell.textContent = _numToToken(gameBoard.getMatrix()[i]);
+			// cell.id = i;
 			cell.addEventListener("click", _markCell);
-			cell.addEventListener("click", game.playTurn); // maybe combine into one event, as if the cell is already marked nothing should happen
-
-			const cellContent = document.createElement("span"); 
-			cellContent.textContent = _numToToken(gameBoard.getMatrix()[i]);
-			cell.appendChild(cellContent);
 
 			gameBoardDiv.appendChild(cell);
 		}
@@ -114,9 +108,10 @@ displayController.render();
  * -    	tried to initially fix by checking to see whether span or div was clicked, and making proper adjustments to parentNode calls
  * - 			was so messy, so i tried ...
  * - when I tried to access game.currentPlayer, I would always get 1 even though i had a function call that should change it to 2
- * - 	apparently when you properties put into returned objects stay constant, unless you specifically modify that object.
- * -  if you modify via a function, it does not modify it, modifys the variable inside the function scope
- * -  this is really confusing, go check out objects_practice example to refresh ur memory
+ * - 		apparently when you properties put into returned objects stay constant, unless you specifically modify that object.
+ * -  	if you modify via a function, it does not modify it, modifys the variable inside the function scope
+ * -  	this is really confusing, go check out objects_practice example to refresh ur memory
  * - figuring out which module each piece of logic should go to
+ * - have two click listeners for cell, not sure how to combine them/which module they should be in
  *
  */
