a8d179573fabf561dddf0d791e62f2fd99f3bcfc
Daniel Ryu <rujjiryu99@gmail.com>
2019-06-20 01:26:37 -0700

    implement tie logic in logic.js

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # tic-tac-toe
 
 Currently building a tic-tac-toe web app woohoo 
+
+</br>
+
+**Languages/Technologies/Libraries used:**
+- JavaScript
+- HTML
+- CSS
+- jQuery (1st time)
+
+</br>
+
+**Key Learning Highlights:**
+- Using JS modules and factory functions to better organize code
+
+</br>
+
+**Technical Problems I faced:**
+  
+</br>
+</br>
+
+This assignment was challenging as heck, but holy I learned so much. Great front-end experience. 
+
+
+
+
--- logic.js
+++ logic.js
@@ -4,106 +4,122 @@
  * X = 1
  */
 
-/* OBJECTS */ 
-
-// Player factory function
-const player = (playerToken) => {
-	const token = playerToken;
-	let score = 0;
-	
-	const markAt = (pos) => {
-		gameBoard.getMatrix()[pos] = token;
-
-	};
-
-	return { markAt };
-}; 
-
-
-// Game module
-const game = (() => {
-	let currentPlayer = 0;
-	const playerO = player(0);
-	const playerX = player(1);	
-	let currentWinState = null; 
-	const winStates = [
-		[0,1,2],
-		[3,4,5],
-		[6,7,8],
-		[0,3,6],
-		[1,4,7],
-		[2,5,8],
-		[0,4,8],
-		[2,4,6] 
-	];	
-
-	const getCurrentPlayer = () => currentPlayer;
-	const getCurrentWinState = () => currentWinState;
-
-	const reset = () => {
-		currentWinState = null;
-		gameBoard.resetBoard();
-	}
-
-	const isGameOver = () => {
-		const boardMatrix = gameBoard.getMatrix();
-
-		let currentState = winStates.filter( // determine current game state, if able 
-			winState => winState.every(
-				pos => boardMatrix[pos] !== -1 && boardMatrix[winState[0]] === boardMatrix[pos]
-			)
-		);
-
-		if (currentState.length !== 0) { // check to see if win state exists in current game state
-			currentWinState = currentState[0]; // update win state
-			return true;
-		} else {
-			return false;
-		}
-	};
-
-	const playTurn = (pos) => {		
-		if (currentPlayer) {
-			playerX.markAt(pos);
-		} else {
-			playerO.markAt(pos);
-		}
-
-		currentPlayer = Number(!currentPlayer);
-	};
-
-	return { 
-		getCurrentPlayer,
-		getCurrentWinState,
-		reset,
-		isGameOver,
-		playTurn };
-})();
-
-
-// GameBoard module
-const gameBoard = (() => {
-	const matrix = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
-	
-	const getMatrix = () => matrix;
-
-	const resetBoard = () => {
-		for (let i = 0; i < matrix.length; i++) {
-			matrix[i] = -1;
-		}
-	};
-
-	return { getMatrix, resetBoard };
-})();
 
 
 $(document).ready(function() {	
-	// DisplayController module
+
+	/**
+	 * Player factory function
+	 * @desc a player that has a x/o token
+	 */
+	const player = (playerToken) => {
+		const token = playerToken;
+		let score = 0;
+		
+		const markAt = (pos) => {
+			gameBoard.getMatrix()[pos] = token;
+
+		};
+
+		return { markAt };
+	}; 
+
+
+	/**
+	 * Game Module
+	 * @desc included vars + fns deal with game state and logic
+	 */
+	const game = (() => {
+		let currentWinState = null; 
+		let currentPlayer = 0;
+		const playerO = player(0);
+		const playerX = player(1);		
+		const winStates = [
+			[0,1,2],
+			[3,4,5],
+			[6,7,8],
+			[0,3,6],
+			[1,4,7],
+			[2,5,8],
+			[0,4,8],
+			[2,4,6] 
+		];	
+
+		const getCurrentPlayer = () => currentPlayer;
+		const getCurrentWinState = () => currentWinState;
+
+		const reset = () => {
+			currentWinState = null;
+			gameBoard.reset();
+		}
+
+		const isGameOver = () => {
+			const boardMatrix = gameBoard.getMatrix();
+
+			let currentState = winStates.filter( // determine current game state, if able 
+				winState => winState.every(
+					pos => boardMatrix[pos] !== -1 && boardMatrix[winState[0]] === boardMatrix[pos]
+				)
+			);
+
+			if (currentState.length !== 0) { // check to see if win state exists in current game state
+				currentWinState = currentState[0]; // update win state
+				return true;
+			} else if (isTie()) {
+				return true 
+			} else {
+				return false;
+			}					
+		};
+
+		const isTie = () => { 
+			return !gameBoard.getMatrix().some(pos => pos === -1); // return true if all cells are filled
+		}
+
+		const playTurn = (pos) => {		
+			currentPlayer ? playerX.markAt(pos) : playerO.markAt(pos);
+
+			currentPlayer = Number(!currentPlayer);
+		};
+
+		return { 
+			getCurrentPlayer,
+			getCurrentWinState,
+			reset,
+			isGameOver,
+			playTurn };
+	})();
+
+
+	/**
+	 * Game Board Module
+	 * @desc contains game board data and fns
+	 */
+	const gameBoard = (() => {
+		const matrix = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+		
+		const getMatrix = () => matrix;
+
+		const reset = () => {
+			for (let i = 0; i < matrix.length; i++) {
+				matrix[i] = -1;
+			}
+		};
+
+		return { getMatrix, reset };
+	})();
+
+
+
+	/**
+	 * Display Controller Module
+	 * @desc included fns manipulate and render DOM 
+	 */
 	const displayController = (() => {
 		let blinker = null;
 		
 		const render = () => {
-			for (let i = 0; i < 9; i++) {
+			for (let i = 0; i < 9; i++) { // render initial empty game board
 				const cell = $("<div>").click(_markCell);
 				cell.text(_numToTokenString(gameBoard.getMatrix()[i]));				
 				cell.addClass("game-cell");
@@ -131,27 +147,33 @@ $(document).ready(function() {
 			
 			if (cell.text() !== "") return;
 
-			cell.text(_numToTokenString(game.getCurrentPlayer()));			
+			cell.text(_numToTokenString(game.getCurrentPlayer()));	// mark cell in GUI			
 			cell.addClass(cell.text() === "X" ? "x-token" : "y-token");	
-			game.playTurn(cell.index());
+			game.playTurn(cell.index());	// mark cell in model
 
-			$(".msg-panel .intro-display").css("display", "none");
+			$(".msg-panel .intro-display").css("display", "none"); 
 			$(".msg-panel .turn-display").text("It's " + (_numToTokenEmoji(game.getCurrentPlayer())) + "'s turn");
 
-			if (game.isGameOver()) {
-				$(".game-board").children().off("click"); 
-				$(".reset-btn").text("New Game");
-				$(".msg-panel .turn-display").text(_numToTokenEmoji(!game.getCurrentPlayer()) + " won!!!");
+			if (game.isGameOver()) _renderGameOver();																			
+		}; 
 
-				blinker = setInterval(_blink, 300);				
-			}	
+		const _renderGameOver = () => {
+			$(".game-board").children().off("click"); 
+			$(".reset-btn").text("New Game");
+			
+			if (game.getCurrentWinState() === null) {
+				$(".msg-panel .turn-display").text("It's a tie... 😅");
+			} else {
+				$(".msg-panel .turn-display").text(_numToTokenEmoji(!game.getCurrentPlayer()) + " won!!!");
+				blinker = setInterval(_blink, 300);	// three winning tokens blink repeatedly 	
+			}											
 		};
 
-		const _blink = () => {			
+		const _blink = () => { 			
 			game.getCurrentWinState().forEach(index => {
-				$(".game-cell").eq(index).toggleClass("hidden");							
+				$(".game-cell").eq(index).toggleClass("hidden"); 
 			}); 
-		}
+		};
 
 		const _numToTokenString = (num) => { 
 			return num === -1 ? "" : num ? "X" : "O";
@@ -173,40 +195,3 @@ $(document).ready(function() {
 
 
 
-/**
- * Things I want to do/change
- * - find different mapping for tokens, I don't like X = 1, O = 2 I want it to be binary
- * - css issue when all cells are initially empty, line-height makes cells too big 
- * - implement proper jQuery in markCell function ($(this) is not working)***
- * 			KINDA fixed, couldn't use this but made jQuery object out of e.target, good enough for now
- * - add tie implementation
- * - clean up isGameOver, condition is kinda iffy don't like how there are unintuitive vars 
- * - make tokens rain after someone wins 
- * - implement hovering effect
- * 
- * Things I learned
- * - modules
- * - factory functions
- * - avoiding populating global namespace 
- * - using jQuery to simplify DOM manipulation (makes code much more succint)
- * 
- * Problems I faced
- * - clicking on a cell wouldn't give me its correct position in respect to its pos in game board children divs.
- * - 		problem was because there are also spans inside the divs, so clicking on span would just return 0 instead of cell index
- * -    	tried to initially fix by checking to see whether span or div was clicked, and making proper adjustments to parentNode calls
- * - 			was so messy, so i tried ...
- * 
- * - when I tried to access game.currentPlayer, I would always get 1 even though i had a function call that should change it to 2
- * - 		apparently when you properties put into returned objects stay constant, unless you specifically modify that object.
- * -  	if you modify via a function, it does not modify it, modifys the variable inside the function scope
- * -  	this is really confusing, go check out objects_practice example to refresh ur memory
- * 
- * - figuring out which module each piece of logic should go to
- * 
- * - have two click listeners for cell, not sure how to combine them/which module they should be in
- * 
- * - can't figure out how to pass in an already declared callback function into .click(), and have access to the targetted DOM node using $(this). 
- * 			I can only do it using e.currentTarget, but I dont know why $(this) isn't working sigh
- * 
- * - not sure why current game state is an array of arrays
- */
